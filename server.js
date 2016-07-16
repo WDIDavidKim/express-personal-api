d0f95f6a481defa561e249c6b11d90e3ed21f1b5
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-16 13:30:35 -0700

    testing seed

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    // woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentation_url: "https://github.com/WDIDavidKim/express-personal-api", // CHANGE ME
     base_url: "https://peaceful-tundra-47993.herokuapp.com/", // CHANGE ME
